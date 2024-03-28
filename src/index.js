784c64fecaae46a66972468bc932d15d230ae780
sabari570 <rsabarisai2000@gmail.com>
2024-03-28 15:57:33 +0530

    we have implemented the redux-persist that stores the cart items in the local storage of the web browser so that even if we reload the cart items dont get refreshed it stays safe. This code remains the same even if we implement this in future apps

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import { BrowserRouter } from "react-router-dom";
 
 // Used for implementing and providing redux state management for the whole app
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { persistor, store } from "./store/store";
+
+// This PersistGate allows us to use the persistStore inside our app, we wrap our app inside the PersistGate
+import { PersistGate } from "redux-persist/integration/react";
 
 // This is how to use React router we wrap the entire App inside the BrowserRouter
 
@@ -15,9 +18,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      {/* We wrap the app inside PersistGate and pass in the persistor parameter we created inside our store */}
+      {/* we have a loading parameter inside which says while loading the data from the local storage what should be displayed */}
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,26 @@ import { compose, legacy_createStore as createStore, applyMiddleware } from "red
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
+// This is used for persisting that mean saving the state object so that the app persists the state of the object
+// even if we relaod the web app
+import { persistStore, persistReducer } from "redux-persist";
+
+// This uses the localStorage of the web browser
+import storage from "redux-persist/lib/storage";
+
+// First we write the configuration object that says what are the we want to persist
+// all these codes are same for any different app only the values to store changes rest everything is the same
+const persistsConfig = {
+    key: 'root',    // here root mean we want to persist the whole thing that starts from the root level
+    storage,
+    // blacklist: ['user'],  // This says what reducers we should blacklist which means dont save in the localstorage, 'user' is the name of the reducer
+    whitelist: ['cart']
+};
+
+// This persistedReducer will be passed to the store
+// It takes in the persistConfig and the rootReducer
+const persistedReducer = persistReducer(persistsConfig, rootReducer);
+
 // Whenever you dispatch an action it first hits the middleWares and only then it goes to the rootReducer
 // to carry out the other purposes
 const middleWares = [logger];
@@ -12,4 +32,8 @@ const middleWares = [logger];
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 // Syntax of the createStore-> createStore( <root_reducer>, <additional default states (mostly used for testing)>, <middleware for the logger> )
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+// pass in the persistedReducer in the place of rootReducer
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+// export the persistStore by passing in the store
+export const persistor = persistStore(store);
